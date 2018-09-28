b54b428f9eccdd77962f27786e336c9a39e78c9e
Aleksander Ambrozkiewicz <aambrozkiewicz@apptension.com>
2018-09-28 21:57:11 +0200

    Add gender field

--- contactlist/serializers.py
+++ contactlist/serializers.py
@@ -5,7 +5,11 @@ from contactlist import models
 
 class ContactSerializer(serializers.ModelSerializer):
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    gender = serializers.SerializerMethodField()
 
     class Meta:
         model = models.Contact
-        fields = ('pk', 'name', 'phone', 'age', 'user',)
+        fields = ('pk', 'name', 'phone', 'age', 'user', 'gender',)
+
+    def get_gender(self, instance):
+        return 'female' if instance.name[-1] == 'a' else 'male'
