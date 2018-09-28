5356e85fb7e818a3763a0bf2f1af3a15e3104571
Aleksander Ambrozkiewicz <aambrozkiewicz@apptension.com>
2018-09-28 21:55:19 +0200

    Object level permission or just a queryset filter

--- contactlist/views.py
+++ contactlist/views.py
@@ -5,5 +5,9 @@ from contactlist import models, serializers
 
 class ContactViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.IsAuthenticated,)
-    queryset = models.Contact.objects.all()
     serializer_class = serializers.ContactSerializer
+
+    def get_queryset(self):
+        return models.Contact.objects.filter(
+            user=self.request.user,
+        )
